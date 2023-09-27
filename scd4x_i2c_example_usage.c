bba712215580a25e36ac80e7f0784e067980d633
edgars <ditfkultura@inbox.lv>
2023-09-27 11:43:11 +0300

    add time stamp

--- scd4x_i2c_example_usage.c
+++ scd4x_i2c_example_usage.c
@@ -1,35 +1,5 @@
-/*
- * Copyright (c) 2021, Sensirion AG
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- *
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * * Neither the name of Sensirion AG nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
-#include <stdio.h>  // printf
+#include <stdio.h>
+#include <time.h> // Add this line to include the time library
 
 #include "scd4x_i2c.h"
 #include "sensirion_common.h"
@@ -71,6 +41,14 @@ int main(void) {
     printf("Waiting for first measurement... (5 sec)\n");
 
     for (;;) {
+        // Get the current timestamp
+        time_t current_time;
+        time(&current_time);
+
+        // Convert the timestamp to a string
+        char timestamp[20]; // Adjust the buffer size as needed
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&current_time));
+
         // Read Measurement if data is available
         bool data_ready_flag = false;
         sensirion_i2c_hal_sleep_usec(100000);
@@ -91,6 +69,7 @@ int main(void) {
         } else if (co2 == 0) {
             printf("Invalid sample detected, skipping.\n");
         } else {
+            printf("Timestamp: %s\n", timestamp);
             printf("CO2: %u ppm\n", co2);
             printf("Temperature: %.2f °C\n", temperature);
             printf("Humidity: %.2f RH\n", humidity);
